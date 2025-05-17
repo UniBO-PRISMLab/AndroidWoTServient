8284553075b2130d1ffc3b3b7a5cc77463ff34fe
Lupss19 <filippoluppi2001@gmail.com>
2025-05-17 19:53:43 +0200

    Added permission for camera and audio

--- app/src/main/java/com/example/testserver/PicAudioActivity.kt
+++ app/src/main/java/com/example/testserver/PicAudioActivity.kt
@@ -3,6 +3,7 @@ package com.example.testserver
 import android.app.Activity
 import android.app.ComponentCaller
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Bundle
@@ -11,6 +12,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class PicAudioActivity : AppCompatActivity() {
 
@@ -18,6 +21,11 @@ class PicAudioActivity : AppCompatActivity() {
     private var imageUri: Uri? = null
     private var audioPath: String? = null
     private var mediaRecorder: MediaRecorder? = null
+    private val CAMERA_REQUEST_CODE = 100
+    private val AUDIO_REQUEST_CODE = 101
+    private lateinit var recordAudioButton: Button
+    private lateinit var stopRecordingButton: Button
+    private lateinit var playAudioButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +34,25 @@ class PicAudioActivity : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
 
         val takePictureButton = findViewById<Button>(R.id.takePictureButton)
-        val recordAudioButton = findViewById<Button>(R.id.recordAudioButton)
-        val stopRecordingButton = findViewById<Button>(R.id.stopRecordingButton)
+        recordAudioButton = findViewById(R.id.recordAudioButton)
+        stopRecordingButton = findViewById(R.id.stopRecordingButton)
+        playAudioButton = findViewById(R.id.playAudioButton)
         val sendButton = findViewById<Button>(R.id.sendButton)
+        playAudioButton.isEnabled = false
+        stopRecordingButton.isEnabled = false
 
-        takePictureButton.setOnClickListener { openCamera() }
-        recordAudioButton.setOnClickListener { startRecording() }
+        takePictureButton.setOnClickListener { checkCameraPermission() }
+        recordAudioButton.setOnClickListener { checkAudioPermission() }
         stopRecordingButton.setOnClickListener { stopRecording() }
         sendButton.setOnClickListener { sendMedia() }
+    }
+
+    private fun checkCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+        } else {
+            openCamera()
+        }
     }
 
     private fun openCamera() {
@@ -54,9 +73,19 @@ class PicAudioActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkAudioPermission() {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), AUDIO_REQUEST_CODE)
+        } else {
+            startRecording()
+        }
+    }
+
     // TODO: CAMBIARE SALVATAGGIO FILE/INVIARE DIRETTAMENTE
     private fun startRecording() {
         try {
+            recordAudioButton.isEnabled = false
+            playAudioButton.isEnabled = false
             val filePath = "${externalCacheDir?.absolutePath}/audio.3gp"
             mediaRecorder = MediaRecorder().apply {
                 setAudioSource(MediaRecorder.AudioSource.MIC)
@@ -68,6 +97,7 @@ class PicAudioActivity : AppCompatActivity() {
             }
             audioPath = filePath
             Toast.makeText(this, "Registrazione Avviata", Toast.LENGTH_SHORT).show()
+            stopRecordingButton.isEnabled = true
         } catch (e: Exception) {
             Toast.makeText(this, "Errore nella Registrazione", Toast.LENGTH_SHORT).show()
         }
@@ -80,11 +110,39 @@ class PicAudioActivity : AppCompatActivity() {
         }
         mediaRecorder = null
         Toast.makeText(this, "Registrazione Terminata", Toast.LENGTH_SHORT).show()
+        recordAudioButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        playAudioButton.isEnabled = true
     }
 
     private fun sendMedia() {
         //TODO: usa WoT per inviare
         Toast.makeText(this, "Invio Media (TODO)", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Permesso fotocamera negato", Toast.LENGTH_SHORT).show()
+                }
+            }
+            AUDIO_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startRecording()
+                } else {
+                    Toast.makeText(this, "Permesso microfono negato", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     companion object {
